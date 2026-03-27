ed6fe690bccffd4d563e010af47f3cd9fcd69e41
Christopher Platt <plattnotpratt>
2026-03-27 08:11:41 -0700

    Updated title to role in on load

--- src/components/Elements/Title.jsx
+++ src/components/Elements/Title.jsx
@@ -1,4 +1,4 @@
-import React,{useRef} from "react";
+import React, { useRef } from "react";
 import { useGSAP } from '@gsap/react';
 import { SplitText } from "gsap/SplitText"
 import gsap from 'gsap';
@@ -6,33 +6,43 @@ import gsap from 'gsap';
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(SplitText);
 
-export default function Title(props){
+export default function Title(props) {
     const title = useRef();
     useGSAP(() => {
-        let split = SplitText.create("h1",{type: 'chars'})
-        gsap.from(split.chars, {
+        gsap.to(title.current.querySelectorAll('.title'), {
             // <- selector text, scoped to this component!
-            opacity: 0,
-            y: 100,
-            ease: "back",
-            duration: 1,
-            stagger: 0.1
-        });},{ 
-            scope: title 
+            yPercent: '+=100', // Increase the y position by 100%
+            ease: 'expo.inOut',
+        });
+    }, {
+        scope: title
     })
-    const {entryTitle, name, description } = props
+    const { entryTitle, name } = props
     return (
         <>
             <style>{style}</style>
-            <header ref={title} className="header">
-                <h1 className="title word">{entryTitle ?? name}</h1>
-                {description ? (<p className="muted">{description}</p>) : null}
+            <header ref={title} className="word">
+                <h1 className="title word-hidden">{entryTitle ?? name}</h1>
+                <h1 className="title word-visible">{entryTitle ?? name}</h1>
             </header>
         </>
     );
 }
 const style = `
-h1.title{
-    text-align: center;
-}
+    h1{
+        text-align: center;
+        width: 100%;
+    }
+    .word{
+        line-height: 0.8;
+        position: relative;
+        text-transform: uppercase;
+        clip-path: polygon(0 2%, 0 98%, 100% 98%, 100% 2%);
+    }
+    .word-hidden {
+        position: absolute;
+        left: 0;
+        top: 0;
+        transform: translate(0, -100%); /* Translate it -100% of its height on the y-axis */
+    }
 `;
